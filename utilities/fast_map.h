e16c1e96a0058e99e4f9c2d6183423fccd87a2e9
Abhishek Dahad <abhishek.dahad@gmail.com>
2026-07-18 19:34:28 -0700

    Define public interface to mimic usage in Core Engine

--- utilities/fast_map.h
+++ utilities/fast_map.h
@@ -25,10 +25,10 @@ Then our price levels would look like this:
 To find the best bid/ask we will use a pointer.
 
 Supporting:
-.begin()
-.erase()
-[] operator
-.find()
+.begin() -> supporting finding the top of the book
+.erase() -> support deleting from anywhere in the book
+[] operator -> support insertion if not existing, support access if existing
+.find() -> support access if existing
 
 
 
@@ -36,17 +36,57 @@ Supporting:
 
 */
 #include "pool.h"
+#include <vector>
+// #include <bitset>
+
+using byte = unsigned char;
 
 template <typename T>
 class FastMap {
+
+    
+
 private:
+    enum class SortType {
+        ASCENDING,
+        DESCENDING
+    };
+
     double tick_size;
     double exp_lower;
     double exp_upper;
-    std::vector<std::vector<T>> data;
+    long best_rest_ptr;
+    SortType sort_type;
+    std::vector<std::vector<std::pair<double, T>>> data;
+    std::vector<byte> bitset
 
 public:
     
+    FastMap() = delete;
+    FastMap(double tick, double lower_bound, double upper_bound, SortType sort) :
+    tick_size(tick), exp_lower(lower_bound), exp_upper(upper_bound), sort_type(sort),
+    data((upper_bound - lower_bound)/tick), bitset((upper_bound - lower_bound)/(8*tick)) {}
+
+    
+    
+    void erase(double price) {
+        //set bitmask off, don't deallocate memory
+    }
+
+    T& operator[](double price) {
+        //check if price exists, if not allocate
+        //then return value
+    }
+
+    std::pair<double, T>* begin() {
+        //return object at the top of the book
+    }
+
+    std::pair<double, T>* find(double price) {
+        //return object pointed to by this price using bin search?
+    }
+
+
 
 
 
