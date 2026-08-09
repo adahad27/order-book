e7396aa837acf0e16ffafa85c0846c9f3746b974
Abhishek <abhishek.dahad@gmail.com>
2026-08-08 20:09:40 -0700

    Add hooks for Intrusive Linked List similar to Boost implementation. Add basic List class to act as substitute for std::list but uses memory pool instead.

--- utilities/pool.h
+++ utilities/pool.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <stdexcept>
 
+
+constexpr uint32_t MAX_LIST_ALLOC_SIZE = UINT32_MAX;
+
 /*
 Ideas to implement:
 1.) Prevent double frees -> Use a bitmap to track which address offsets are free. The size of the
@@ -9,6 +12,10 @@ bitmap is (num_objects / 8), so relatively efficient memory storage
 Or we could use atomic read-modify-writes to implement a lock-free stack.
 
 */
+struct ILLHook {
+    ILLHook* next{nullptr};
+    ILLHook* prev{nullptr};
+};
 
 template <typename T>
 class Pool {
@@ -86,4 +93,97 @@ public:
         delete[] data;
         
     }
+};
+
+template <typename T>
+/*
+TODO: Enforce a concept to ensure that T has inherited
+from ILLHook.
+*/
+class List {
+private:
+    
+    uint32_t _size;
+    Pool<T> pool;
+    T* head {nullptr};
+    T* tail {nullptr};
+public:
+    List() : pool(MAX_LIST_ALLOC_SIZE) {}
+
+    bool push_back(const T& obj) {
+        T* new_node = pool.alloc_obj();
+        *new_node = obj;
+        
+        if (!new_node) return false;
+
+        if(_size == 0) {
+            head = new_node;
+            tail = new_node;
+            
+        } else {
+            tail->next = new_node;
+            new_node->prev = tail;
+            tail = new_node;
+        }
+
+        ++_size;
+        return true;
+    }
+
+    bool push_front(const T& obj) {
+        T* new_node = pool.alloc_obj();
+        *new_node = obj;
+        
+        if (!new_node) return false;
+
+        if(_size == 0) {
+            head = new_node;
+            tail = new_node;
+            
+        } else {
+            head->prev = new_node;
+            new_node->next = head;
+            head = new_node;
+        }
+        ++_size;
+        return true;
+    }
+
+    void remove(T* obj) {
+        if(!obj) {
+            return;
+        }
+
+        if(obj == head) {
+            head = obj->next;
+            head->prev = nullptr;
+        } else if (obj == tail) {
+            tail = obj->prev;
+            tail->next = nullptr;
+        } else {
+            T* prev_node = obj->prev;
+            T* next_node = obj->next;
+            prev_node->next = next_node;
+            next_node->prev = prev_node;
+            
+        }
+        obj->next = nullptr;
+        obj->prev = nullptr;
+
+        pool.free_obj(obj);
+        --_size;
+    }
+
+    uint32_t size() {
+        return _size;
+    }
+
+    T* front() {
+        return head;
+    }
+
+    T* back() {
+        return tail;
+    }
+
 };
--- utilities/types.h
+++ utilities/types.h
@@ -3,44 +3,60 @@
 #include <iostream>
 #include <list>
 #include <map>
-enum class OrderType {
+#include "pool.h"
+
+enum class OrderType : uint8_t{
     BID,
     ASK
 };
 
-enum class SortType { ASCENDING, DESCENDING };
+enum class SortType : uint8_t { ASCENDING, DESCENDING };
 
-enum class OrderSubType {
+enum class OrderSubType : uint8_t {
     MARKET, // buy/sell at whatever the best current price is on the opposite side
     LIMIT, // buy/sell at the limit that is set in the order or a better price
     STOP_LOSS, // sell at best market price if the last traded price is at or below some level
-    FILL_OR_KILL, // fill the position in it's entirety if possible, else cancel it
-    IMMED_OR_CANCEL //fill or partially fill the order if possible, else cancel it
+    FILL_OR_KILL, // fill the position in it's entirety if possible, else cancel it, these can either be done at market price or at limit price
+    IMMED_OR_CANCEL //fill or partially fill the order if possible, cancel remaining balance, these can either be done at market price or at limit price
 };
 
-enum class EventType {
+enum class EventType : uint8_t {
     INC_SELL,
     INC_BUY,
     ORDER_EXEC
 };
 
-enum class JobType {
+enum class JobType : uint8_t {
     ADD,
     CANCEL,
     MODIFY
 };
 
-struct Order {
-    std::string ticker;
+
+
+struct Order : ILLHook { //NOTE: Even if this is rearranged, there will still be 6 bytes of padding
+    //This ends up being 88 bytes wide
+    /*
+    TODO: Get rid of 8 bytes by removing price from the order structure. Also remove
+    user_id and create a separate map.
+    1.) Remove price from struct => -8
+    2.) Remove user_id and create separate map => -32
+    3.) Replace type of ticker with char array of 8 bytes => -24
+    4.) Add front/back pointer for intrusive linked list => +16
+    5.) Replace u32 to u16 for quantity => -2
+    6.) Remove order_id => -4
+    
+    */
+    std::string ticker; //8
     std::string user_id;
-    uint32_t order_id;
-    OrderType order_type;
-    OrderSubType order_subtype;
-    double price;
-    uint32_t quantity;    
+    uint32_t order_id; //4
+    OrderType order_type;// 1
+    OrderSubType order_subtype;// 1
+    double price; // 8
+    uint32_t quantity; // 4    
 };
 
-struct FilledOrder {
+struct FilledOrder { //Should be 56 bytes wide
     std::string ticker;
     std::string user_id;
     std::string bidder_id;
